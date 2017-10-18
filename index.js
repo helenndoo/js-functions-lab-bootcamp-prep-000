012143c1cda08fa65a10235b70063bf0d42843f3
Helen Du <helendu.614@gmail.com>
2017-10-18 21:37:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
 function happyHolidays() {
-  return string("Happy holidays!")
-}
-
-function happyHolidaysTo() {
-  return 'Happy holidays, ${name}!'
-}
-
-function happyHolidaysTo() {
-  return string('Happy ${holiday}, ${name}!')
-}
-
-function holidayCountdown() {
-  return string('It\'s ${days} days until ${holiday}!'
+  return ("Happy holidays!")
 }
