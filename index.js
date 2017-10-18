511c7343d3a7066a09ec6155169e1f780fa6739b
Helen Du <helendu.614@gmail.com>
2017-10-18 21:55:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ function happyHolidays() {
 }
 
 
-function happyHolidaysTo(name) {
-  return ("Happy holidays," (name)!)
-}
+//function happyHolidaysTo(name) {
+  //return ("Happy holidays," (name)!)
+//}
 
 
-function happyHolidaysTo(holiday, name) {
-  return ("Happy" ${holiday}, ${name}!)
-}
+//function happyHolidaysTo(holiday, name) {
+//  return ("Happy" ${holiday}, ${name}!)
+//}
 
 
-function holidayCountdown(days, holiday) {
-  return ("It\'s" ${days} "days until "${holiday}!)
-}
+//function holidayCountdown(days, holiday) {
+//  return ("It\'s" ${days} "days until "${holiday}!)
+// }
